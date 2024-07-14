c9ee31b017c47386a6859f384911165ece16ff49
Bouhm-Yassine <yassinbouhm@gmail.com>
2024-07-14 23:34:56 +0100

    t global scopea

---
+++ esgauth/__init__.py
@@ -0,0 +1,2 @@
+# from .azure_ad_auth import AzureADAuth
+# azure_auth = AzureADAuth("87c712c6-82bd-4510-9b9f-12d4ca8a9259", "https://login.microsoftonline.com/d6f9c526-4b38-42bb-b2fd-491f492591db")
--- esgauth/decorator.py
+++ esgauth/decorator.py
@@ -1,5 +1,5 @@
 from functools import wraps
-
+from flask import g
 from .azure_ad_auth import AzureADAuth
 
 def token_required(f):
@@ -7,8 +7,9 @@ def token_required(f):
     def decorated_function(*args, **kwargs):
         try:
             # Decode token and store it in the request object
-            decoded_token = AzureADAuth.decode_token()
-
+            # decoded_token = AzureADAuth.decode_token()
+            decoded_token = "ABABABAB"
+            g.decoded_token = decoded_token
             print('========= decoded_token', decoded_token)
             
 
