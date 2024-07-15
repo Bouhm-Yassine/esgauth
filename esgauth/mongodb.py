e2000b8f3209ce94e41a1f79c0d6bc721f607c21
Bouhm-Yassine <yassinbouhm@gmail.com>
2024-07-15 10:57:54 +0100

    lazy initilze mongo db

--- esgauth/mongodb.py
+++ esgauth/mongodb.py
@@ -1,18 +1,29 @@
 from pymongo import MongoClient
-from flask import current_app as app
+from flask import has_app_context, current_app as app
 class MongoDB:
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, ):
         if cls._instance is None:
             cls._instance = super(MongoDB, cls).__new__(cls)
-
-            cls._instance.client = MongoClient(app.config['MONGO_URI'])
-            cls._instance.db = cls._instance.client.get_default_database()
-
         return cls._instance
     
+    @classmethod
+    def _initialize(cls):
+        if not has_app_context():
+            raise RuntimeError("Application context required for MongoDB initialization")
+        
+        if cls._client is None or cls._db is None:
+            cls._client = MongoClient(app.config['MONGO_URI'])
+            cls._db = cls._client.get_default_database()
 
     @classmethod
+    def get_db(cls):
+        if cls._db is None:
+            cls._initialize()
+        return cls._db
+    
+    @classmethod
     def get_user(cls, query):
-        return cls._instance.db.users.find_one(query)
+        db = cls.get_db()
+        return db.users.find_one(query)
